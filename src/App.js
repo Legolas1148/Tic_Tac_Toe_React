c80c3f6ad38b4aab35ef9504f3ea646f04569dfc
Legolas1148 <niteeshrawal1@gmail.com>
2025-03-25 11:52:59 -0400

    Intial Setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+export default function Square() {
+  return 
+  <>
+    <div clasName = "board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>  
+    </div>
+    <div clasName = "board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>  
+    </div>
+    <div clasName = "board-row">
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>  
+    </div>
+  </>;
+}
