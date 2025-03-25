1f36542d10ed7743e2e9e5161b68b119120123e4
Legolas1148 <niteeshrawal1@gmail.com>
2025-03-25 14:26:37 -0400

    Fixed Game logic error.

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function Board({xIsNext, squares, onPlay}) {
 export default function Game()
 {
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([{squares: Array(9).fill(null)}]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSqaures)
@@ -69,6 +69,8 @@ export default function Game()
     setXIsNext(!xIsNext);
     setHistory([...history, nextSqaures]);
   }
+
+  
 
   return (
     <div className="game">
