06790cb2bf8d8383061049fca5ff35ff2ab1ba0c
Legolas1148 <niteeshrawal1@gmail.com>
2025-03-25 12:08:53 -0400

    Basic Square Defined

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-export default function Square() {
-  return 
+import {useState} from "react";
+
+function Sqaure()
+{
+  const [value, setValue] = useState(null);
+  
+  function handleClick()
+  {
+    setValue('X');
+  }
+
+  return (<button className="square" onClick={handleClick}>{value}</button>);
+}
+
+export default function Board() {
+  return( 
   <>
-    <div clasName = "board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>  
+    <div className = "board-row">
+      <Sqaure />
+      <Sqaure />
+      <Sqaure />  
     </div>
-    <div clasName = "board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>  
+    <div className = "board-row">
+      <Sqaure />
+      <Sqaure />
+      <Sqaure /> 
     </div>
-    <div clasName = "board-row">
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>  
+    <div className = "board-row">
+      <Sqaure />
+      <Sqaure />
+      <Sqaure /> 
     </div>
-  </>;
+  </>
+  );
 }
