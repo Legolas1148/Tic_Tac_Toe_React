34305249234838323bc24ab769370689748a7806
Legolas1148 <niteeshrawal1@gmail.com>
2025-03-25 12:33:20 -0400

    Added state to squares and lifted state to board.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import {useState} from "react";
 
-function Sqaure()
-{
-  const [value, setValue] = useState(null);
-  
-  function handleClick()
-  {
-    setValue('X');
-  }
-
-  return (<button className="square" onClick={handleClick}>{value}</button>);
+function Sqaure({value, onSquareClick})
+{  
+  return (<button className="square" onclick = {onSquareClick}>{value}</button>);
 }
 
 export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick()
+  {
+    const nextSqaures = squares.slice();
+    nextSqaures[0] = 'X';
+    setSquares(nextSqaures);
+  }
+  
   return( 
   <>
     <div className = "board-row">
-      <Sqaure />
-      <Sqaure />
-      <Sqaure />  
+      <Sqaure value = {squares[0]} onSquareClick ={handleClick}/>
+      <Sqaure value = {squares[1]} onSquareClick ={handleClick}/>
+      <Sqaure value = {squares[2]} onSquareClick ={handleClick}/>  
     </div>
     <div className = "board-row">
-      <Sqaure />
-      <Sqaure />
-      <Sqaure /> 
+      <Sqaure value = {squares[3]} onSquareClick ={handleClick}/>
+      <Sqaure value = {squares[4]} onSquareClick ={handleClick}/>
+      <Sqaure value = {squares[5]} onSquareClick ={handleClick}/> 
     </div>
     <div className = "board-row">
-      <Sqaure />
-      <Sqaure />
-      <Sqaure /> 
+      <Sqaure value = {squares[6]} onSquareClick ={handleClick}/>
+      <Sqaure value = {squares[7]} onSquareClick ={handleClick}/>
+      <Sqaure value = {squares[8]} onSquareClick ={handleClick}/> 
     </div>
   </>
   );
